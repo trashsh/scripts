91e4531340731a87b2795b12f53cf71f54599966
root <root@alixi.ru>
2019-03-06 16:34:06 +0300

    06/03/2019 16:34:00

---
+++ include/inc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source /etc/profile
+source $SCRIPTS/functions/functions.sh
+source $SCRIPTS/external_scripts/dev-shell-essentials-master/dev-shell-essentials.sh
+#source $SCRIPTS/functions/backups.sh
+#source $SCRIPTS/functions/mysql.sh
+#source $SCRIPTS/functions/users.sh
+#source $SCRIPTS/functions/input_functions.sh
+#source $SCRIPTS/functions/do.sh
+#source $SCRIPTS/functions/all_functions.sh
+#source $SCRIPTS/external_scripts/dev-shell-essentials-master/dev-shell-essentials.sh
