d8b2174be729576b66b78c66745cff4c5e96949e
Gothundead <r@gothundead.ru>
2019-03-06 10:40:05 +0300

    06/03/2019 10:40:05 - объединил функции

--- include/include.sh
+++ include/include.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 source /etc/profile
-source $SCRIPTS/functions/archive.sh
 source $SCRIPTS/functions/backups.sh
 source $SCRIPTS/functions/mysql.sh
-source $SCRIPTS/functions/other.sh
 source $SCRIPTS/functions/users.sh
 source $SCRIPTS/functions/input.sh
-source $SCRIPTS/functions/final.sh
 source $SCRIPTS/functions/do.sh
 source $SCRIPTS/functions/all_functions.sh
 source $SCRIPTS/external_scripts/dev-shell-essentials-master/dev-shell-essentials.sh
