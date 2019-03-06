1945a8067e7f48b010687a16352a17826a24b8b5
root <root@alixi.ru>
2019-03-06 17:30:36 +0300

    06/03/2019 17:30:36

--- functions/functions.sh
+++ functions/functions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#users
+####################################users########################
 declare -x -f userAddSystem_input
 declare -x -f userAddSystem
 declare -x -f viewGroupUsersAccessAll
@@ -39,9 +39,12 @@ declare -x -f userExistInGroup
 #3 - группа $2 не существует
 #4 - пользователь $1 не входит в группу $2
 
+####################################mysql########################
+declare -x -f dbSetMyCnfFile
 
 
-
+####################################Архивация########################
+declare -x -f tarFile
 
 #######################################USERS##########################################
 #Добавление системного пользователя - ввод данных
@@ -215,7 +218,7 @@ userAddSystem()
 
                                          echo "$1:$6" | -s chpasswd
 
-                                        dbSetMyCnfFile $1 $6
+                                        dbSetMyCnfFile $1 $1 $6
                                         mkdirWithOwn $2/.backups $1 $4 777
                                         mkdirWithOwn $2/.backups/auto $1 $4 755
                                         mkdirWithOwn $2/.backups/manually $1 $4 755
@@ -508,6 +511,401 @@ userExistInGroup() {
 	else
 	#Параметры запуска отсутствуют
 		echo -e "${COLOR_RED} Отсутствуют необходимые параметры в функции ${COLOR_GREEN}\"UserExistInGroup\"${COLOR_RED} ${COLOR_NC}"
+		return 1
+	#Параметры запуска отсутствуют (конец)
+	fi
+	#Конец проверки существования параметров запуска скрипта
+}
+
+
+#############################################MYSQL###############################################
+#Смена пароля и создание файла ~/.my.cnf (только файл)
+###input:
+#$1-system user ;
+#$2-mysql user ;
+#$3-mysql password ;
+#return:
+#0 - выполнено успешно,
+#1 - отсутствуют параметры,
+#2 - пользователь не существует
+#3 - после выполнения функции файл my.cnf не найден
+#4 - пользователь mysql $2 - не существует
+dbSetMyCnfFile() {
+	#Проверка на существование параметров запуска скрипта
+	if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]
+	then
+	#Параметры запуска существуют
+		#Проверка существования системного пользователя "$1"
+			grep "^$1:" /etc/passwd >/dev/null
+			if [ $? -eq 0 ]
+			then
+			#Пользователь $1 существует
+                #Проверка на существование пользователя mysql "$2"
+                if [[ ! -z "`mysql -qfsBe "SELECT User FROM mysql.user WHERE User='$2'" 2>&1`" ]];
+                then
+                #Пользователь mysql "$2" существует
+                        #если файл для бэкапа существует
+                        if [ -f "$HOMEPATHWEBUSERS"/"$1"/"my.cnf" ] ; then
+                                backupImportantFile $1 "my.cnf"  $HOMEPATHWEBUSERS/$1/.my.cnf
+                                sudo sed -i "s/.*password=.*/password=$3/" $HOMEPATHWEBUSERS/$1/.my.cnf
+                        #если файл для бэкапа не существует
+                        else
+                            sudo touch $HOMEPATHWEBUSERS/$1/.my.cnf
+                            sudo chmod 666 $HOMEPATHWEBUSERS/$1/.my.cnf
+                            sudo cat $HOMEPATHWEBUSERS/$1/.my.cnf | sudo grep $HOMEPATHWEBUSERS
+                                                {
+                                        echo '[mysqld]'
+                                        echo 'init_connect=‘SET collation_connection=utf8_general_ci’'
+                                        echo 'character-set-server=utf8'
+                                        echo 'collation-server=utf8_general_ci'
+                                        echo ''
+                                        echo '[client]'
+                                        echo 'default-character-set=utf8'
+                                        echo 'user='$2
+                                        echo 'password='$3
+                                        } > $HOMEPATHWEBUSERS/$1/.my.cnf
+                                        sudo chmod 600 $HOMEPATHWEBUSERS/$1/.my.cnf
+                                        sudo chown $1:users $HOMEPATHWEBUSERS/$1/.my.cnf
+
+                                        backupImportantFile $1 "my.cnf"  $HOMEPATHWEBUSERS/$1/.my.cnf
+
+                            #Финальная проверка существования файла "$HOMEPATHWEBUSERS/$1/.my.cnf"
+                            if [ -f $HOMEPATHWEBUSERS/$1/.my.cnf ] ; then
+                                #Файл "$HOMEPATHWEBUSERS/$1/.my.cnf" существует
+                                return 0
+                                #Файл "$HOMEPATHWEBUSERS/$1/.my.cnf" существует (конец)
+                            else
+                                #Файл "$HOMEPATHWEBUSERS/$1/.my.cnf" не существует
+                                echo -e "${COLOR_RED}Файл ${COLOR_GREEN}\"\"${COLOR_RED} не существует${COLOR_NC}"
+                                return 3
+                                #Файл "$HOMEPATHWEBUSERS/$1/.my.cnf" не существует (конец)
+                            fi
+                            #Финальная проверка существования файла "$HOMEPATHWEBUSERS/$1/.my.cnf" (конец)
+                        fi
+                #Пользователь mysql "$2" существует (конец)
+                else
+                #Пользователь mysql "$2" не существует
+                    echo -e "${COLOR_RED}Пользователь mysql ${COLOR_GREEN}\"$2\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"dbSetMyCnfFile\" ${COLOR_NC}"
+                    return 4
+                #Пользователь mysql "$2" не существует (конец)
+                fi
+                #Конец проверки на существование пользователя mysql "$2"
+
+
+			#Пользователь $1 существует (конец)
+			else
+			#Пользователь $1 не существует
+				echo -e "${COLOR_RED}Пользователь ${COLOR_GREEN}\"$1\"${COLOR_RED} не существует. Ошибка выполнения скрипта dbSetMyCnfFile${COLOR_NC}".
+				return 2
+			#Пользователь $1 не существует (конец)
+			fi
+		#Конец проверки существования системного пользователя $1
+	#Параметры запуска существуют (конец)
+	else
+	#Параметры запуска отсутствуют
+		echo -e "${COLOR_RED} Отсутствуют необходимые параметры в функции ${COLOR_GREEN}\"dbSetMyCnfFile\"${COLOR_RED} ${COLOR_NC}"
+		return 1
+	#Параметры запуска отсутствуют (конец)
+	fi
+	#Конец проверки существования параметров запуска скрипта
+}
+
+##########################################BACKUPS################################################
+#Создание бэкапа в папку BACKUPFOLDER_IMPORTANT
+###Полностью готово. Не трогать. 06.03.2019 г.
+###input:
+#$1-user ;
+#$2-destination_folder (название катала в $BACKUPFOLDER_IMPORTANT);
+#$3-архивируемый файл ;
+###return:
+#0 - выполнено успешно
+#1 - отпутствуют параметры
+#2 - пользователь $2 - не существует
+#3 - файл $3 не найден
+#4 - в процессе архивации возникла ошибка
+backupImportantFile() {
+	dt=$DATETIMEFORMAT
+	#Проверка на существование параметров запуска скрипта
+	if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ]
+	then
+	#Параметры запуска существуют
+	    #Проверка существования системного пользователя "$1"
+	    	grep "^$1:" /etc/passwd >/dev/null
+	    	if  [ $? -eq 0 ]
+	    	then
+	    	#Пользователь $1 существует
+                #Проверка существования файла "$3"
+                if [ -f $3 ] ; then
+                    #Файл "$3" существует
+                    #Проверка существования каталога "$BACKUPFOLDER_IMPORTANT"/"$2"/"$1"
+                    if ! [ -d "$BACKUPFOLDER_IMPORTANT"/"$2"/"$1" ] ; then
+                        sudo mkdir -p $BACKUPFOLDER_IMPORTANT/$2/$1
+                    fi
+                    #Проверка существования каталога "$BACKUPFOLDER_IMPORTANT"/"$2"/"$1" (конец)
+                        tarFile $3 $BACKUPFOLDER_IMPORTANT/$2/$1/$2_$1_$dt.tar.gz str silent rewrite $1 users 644 && return 0 || return 4
+                    #Файл "$3" существует (конец)
+                else
+                    #Файл "$3" не существует
+                    echo -e "${COLOR_RED} Файл ${COLOR_YELLOW}\"$3\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_YELLOW}\"backupImportantFile\"${COLOR_NC}"
+                    return 3
+                    #Файл "$3" не существует (конец)
+                fi
+                #Конец проверки существования файла "$3"
+	    	#Пользователь $1 существует (конец)
+	    	else
+	    	#Пользователь $1 не существует
+	    	    echo -e "${COLOR_RED}Пользователь ${COLOR_GREEN}\"$1\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"backupImportantFile\"${COLOR_NC}"
+	    		return 2
+	    	#Пользователь $1 не существует (конец)
+	    	fi
+	    #Конец проверки существования системного пользователя $1
+	#Параметры запуска существуют (конец)
+	else
+	#Параметры запуска отсутствуют
+		echo -e "${COLOR_RED} Отсутствуют необходимые параметры в функции ${COLOR_GREEN}\"backupImportantFile\"${COLOR_RED} ${COLOR_NC}"
+		return 1
+	#Параметры запуска отсутствуют (конец)
+	fi
+	#Конец проверки существования параметров запуска скрипта
+}
+
+#######################################Архивация######################################
+
+#архивация файла
+###Полностью готово. Не трогать. 06.03.2019 г.
+###input:
+#$1-путь к исходному файлу ;
+#$2-Путь к конечному архиву ;
+#$3 - mode(str, str_rem, nostr, nostr_rem);
+#$4 - mode (full_info-папка не создается, но показывается и успешный результат,
+#           querry-запрашивается создание каталога,
+#           error_only-выводятся только ошибки,
+#           silent - создается папка);
+#$5 - mode(rewrite/norewrite);
+#$6-Необязательно:имя владельца ;
+#$7-Необязательно:группа владельца ;
+#$8-Необязательно:права доступа ;)
+###return
+#0 - выполнено успешно,
+#1 - отсутствуют параметры,
+#2 - отсутствует исходный файл,
+#3 - отсутствует каталог назначения,
+#4 -неправильно указаны параметры (str, str_rem, nostr, nostr_rem),
+#5 - неправильно указаны параметры  mode (full_info,querry,silent,error_only), ошибка значения переменной $showSuccessResult
+#6 - не существует пользователь системы,
+#7 - не существует группа,
+#8 - финальная проверка существования файла завершилась с ошибкой
+#9 - ошибка передачи параметра Mode (rewrite/norewrite),
+#10 - файл $2 уже существует, а из-за параметра 9(norewrite) не будет перезаписан
+tarFile() {
+	#Проверка на существование параметров запуска скрипта
+	if [ -n "$1" ] && [ -n "$2" ] && [ -n "$3" ] && [ -n "$4" ] && [ -n "$5" ]
+	then
+	#Параметры запуска существуют
+
+	#Проверка существования файла "$2" и параметра перезаписи файла назначения
+	if [ -f $2 ] ; then
+	    #Файл "$2" существует
+
+	        case "$5" in
+                rewrite)
+                    echo -e "${COLOR_YELLOW}Файл ${COLOR_GREEN}\"$2\"${COLOR_YELLOW} уже существует и будет перезаписан. Выполняется архивация файла ${COLOR_GREEN}\"$1\"${COLOR_YELLOW}  ${COLOR_NC}"
+                    ;;
+                norewrite)
+                    echo -e "${COLOR_RED}Файл ${COLOR_GREEN}\"$2\"${COLOR_RED} уже существует и не будет перезаписан. Операция архивирования прервана${COLOR_NC}";
+                    return 10
+                    ;;
+                *)
+                    echo -e "${COLOR_RED}Ошибка передачи параметра ${COLOR_GREEN}\"mode (rewrite/norewrite)\"${COLOR_RED} в функцию ${COLOR_GREEN}\"tarFile\"${COLOR_NC}";
+                    return 9
+                    ;;
+            esac
+
+	    #Файл "$2" существует (конец)
+	fi
+	#Конец проверки существования файла "$2"
+
+		#Проверка существования файла "$1"
+		if [ -f $1 ] ; then
+		    case "$4" in
+		        full_info)
+		            #Проверка существования каталога "`dirname $2`"
+		            if [ -d `dirname $2` ] ; then
+		                #Каталог "`dirname $2`" существует
+		                showSuccessResult=1 #Показывать ли успешный результат
+		                folder=`dirname $2`
+		                #Каталог "`dirname $2`" существует (конец)
+		            else
+		                #Каталог "`dirname $2`" не существует
+		                echo -e "${COLOR_RED}Каталог назначения ${COLOR_GREEN}\"`dirname $2`\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+		                return 3
+		                #Каталог "`dirname $2`" не существует (конец)
+		            fi
+		            #Конец проверки существования каталога "`dirname $2`"
+
+		            ;;
+		        silent)
+		            showSuccessResult=0; #Показывать ли успешный результат
+		            folder=`dirname $2`;
+		            sudo mkdir -p $folder
+		            ;;
+		        querry)
+		             #Проверка существования каталога "`dirname $2`"
+		             showSuccessResult=1 #Показывать ли успешный результат
+		            if [ -d `dirname $2` ] ; then
+		                #Каталог "`dirname $2`" существует
+		                folder=`dirname $2`
+		                #Каталог "`dirname $2`" существует (конец)
+		            else
+		                #Каталог "`dirname $2`" не существует
+		                echo -n -e "${COLOR_RED}Каталог ${COLOR_YELLOW}`dirname $2`${COLOR_RED} не существует. ${COLOR_YELLOW} Введите ${COLOR_BLUE}\"y\"${COLOR_NC}${COLOR_YELLOW} для его создания, для отмены операции - ${COLOR_BLUE}\"n\"${COLOR_NC}: "
+		                	while read
+		                	do
+		                    	echo -n ": "
+		                    	case "$REPLY" in
+		                	    	y|Y)
+		                	    	    folder=`dirname $2`;
+		                                sudo mkdir -p $folder;
+		                                #echo -e "${COLOR_GREEN}Файл ${COLOR_YELLOW}\"$1\"${COLOR_GREEN} успешно заархивирован в ${COLOR_YELLOW}\"$2\"${COLOR_GREEN}  ${COLOR_NC}";
+		                                break
+		                                ;;
+		                		    n|N)
+		                		        echo -e "${COLOR_RED}Создание каталога ${COLOR_GREEN}\"`dirname $2`\"${COLOR_RED} отменено пользователем. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+		                                return 3;
+		                			    break;;
+		                	    esac
+		                	done
+
+		                #Каталог "`dirname $2`" не существует (конец)
+		            fi
+		            #Конец проверки существования каталога "`dirname $2`"
+		            ;;
+		    	error_only)
+		    		#Проверка существования каталога "`dirname $2`"
+		            if [ -d `dirname $2` ] ; then
+		                #Каталог "`dirname $2`" существует
+		                showSuccessResult=0 #Показывать ли успешный результат
+		                folder=`dirname $2`
+		                #Каталог "`dirname $2`" существует (конец)
+		            else
+		                #Каталог "`dirname $2`" не существует
+		                echo -e "${COLOR_RED}Каталог назначения ${COLOR_GREEN}\"`dirname $2`\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+		                return 3
+		                #Каталог "`dirname $2`" не существует (конец)
+		            fi
+		            #Конец проверки существования каталога "`dirname $2`"
+		    		;;
+		    	*)
+		    	    echo -e "${COLOR_RED}Ошибка передачи параметра ${COLOR_GREEN}\"mode (full_info,querry,error_only)\"${COLOR_RED} в функцию ${COLOR_GREEN}\"tarFile\"${COLOR_NC}";
+		    	    return 5
+		    	    ;;
+		    esac
+
+
+            #тип архивации (с сохранением структуры, дополнительно с удалением исходника, без структуры, доп. с удалением исходника)
+		    case "$3" in
+		        str)
+                    sudo tar -czpf $2 -P $1
+		            ;;
+		        str_rem)
+                    sudo tar -czpf $2 -P $1 --remove-files
+		            ;;
+		    	nostr)
+                    cd `dirname $1` && sudo tar -czpf $2 `basename $1`
+		    		;;
+		    	nostr_rem)
+                    cd `dirname $1` && sudo tar -czpf $2 `basename $1` --remove-files
+		    		;;
+		    	*)
+		    	    echo -e "${COLOR_RED}Ошибка передачи параметра ${COLOR_GREEN}\"mode (str, str_rem, nostr, nostr_rem)\"${COLOR_RED} в функцию ${COLOR_GREEN}\"tarFile\"${COLOR_NC}";
+		    	    return 4
+		    	    ;;
+		    esac
+		    #тип архивации (с сохранением структуры, дополнительно с удалением исходника, без структуры, доп. с удалением исходника) (конец)
+
+            #Если указаны дополнительные параметры
+            #Проверка на существование параметров запуска скрипта
+            if [ -n "$6" ] && [ -n "$7" ] && [ -n "$8" ]
+            then
+            #Параметры запуска существуют
+                #Проверка существования системного пользователя "$6"
+                	grep "^$6:" /etc/passwd >/dev/null
+                	if  [ $? -eq 0 ]
+                	then
+                	#Пользователь $6 существует
+                		#Проверка существования системной группы пользователей "$7"
+                		if grep -q $7 /etc/group
+                		    then
+                		        #Группа "$67 существует
+                                sudo chmod $8 $2
+                                sudo chown $6:$7 $2
+                		        #Группа "$7" существует (конец)
+                		    else
+                		        #Группа "$7" не существует
+                		        echo -e "${COLOR_RED}Группа ${COLOR_GREEN}\"$7\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+                				return 7
+                				#Группа "$7" не существует (конец)
+                		    fi
+                		#Конец проверки существования системной группы пользователей $7
+
+
+                	#Пользователь $6 существует (конец)
+                	else
+                	#Пользователь $6 не существует
+                	    echo -e "${COLOR_RED}Пользователь ${COLOR_GREEN}\"$6\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+                	    return 6
+
+                	#Пользователь $6 не существует (конец)
+                	fi
+                #Конец проверки существования системного пользователя $6
+            #Параметры запуска существуют (конец)
+            else
+            #Параметры запуска отсутствуют
+                return 0
+            #Параметры запуска отсутствуют (конец)
+            fi
+            #Конец проверки существования параметров запуска скрипта
+
+		    #финальная проверка существования архива $2
+		    #Проверка существования файла "$2"
+		    if [ -f $2 ] ; then
+		        #Файл "$2" существует
+		        case $showSuccessResult in
+		            0)
+                        return 0
+		                ;;
+		            1)
+                        echo -e "${COLOR_GREEN}Файл ${COLOR_YELLOW}\"$1\"${COLOR_GREEN} успешно заархивирован в ${COLOR_YELLOW}\"$2\"${COLOR_GREEN}  ${COLOR_NC}";
+                        return 0
+		                ;;
+		        	*)
+		        	    echo -e "${COLOR_RED}Ошибка значения переменной ${COLOR_GREEN}\"showSuccessResult\"${COLOR_RED} в функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}";
+		        	    echo $showSuccessResult;
+		        	    return 6
+		        	    ;;
+		        esac
+		        #Файл "$2" существует (конец)
+		    else
+		        #Файл "$2" не существует
+		        echo -e "${COLOR_RED}Файл ${COLOR_GREEN}\"$2\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"\"${COLOR_NC}"
+
+		        #Файл "$2" не существует (конец)
+		    fi
+		    #Конец проверки существования файла "$2"
+            #финальная проверка существования архива $2 (конец)
+
+		else
+		    #Файл "$1" не существует
+		    echo -e "${COLOR_RED}Файл ${COLOR_GREEN}\"$1\"${COLOR_RED} не существует. Ошибка выполнения функции ${COLOR_GREEN}\"tarFile\"${COLOR_NC}"
+            return 2
+		    #Файл "$1" не существует (конец)
+		fi
+		#Конец проверки существования файла "$1"
+
+	#Параметры запуска существуют (конец)
+	else
+	#Параметры запуска отсутствуют
+		echo -e "${COLOR_RED} Отсутствуют необходимые параметры в функции ${COLOR_GREEN}\"tarFile\"${COLOR_RED} ${COLOR_NC}"
 		return 1
 	#Параметры запуска отсутствуют (конец)
 	fi
